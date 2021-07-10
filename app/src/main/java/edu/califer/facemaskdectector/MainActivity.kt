a72b224bd3719fde651ecc45f13c2156f066b3ec
ARPIT KUMAR TAPASWI <arpit.tapaswi@live.com>
2021-07-10 18:02:03 +0530

    Designed the Face Mask Detector application

--- app/src/main/java/edu/califer/facemaskdectector/MainActivity.kt
+++ app/src/main/java/edu/califer/facemaskdectector/MainActivity.kt
@@ -1,11 +1,319 @@
 package edu.califer.facemaskdectector
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.media.Image
+import android.os.Build
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
+import com.google.common.util.concurrent.ListenableFuture
+import edu.califer.facemaskdectector.databinding.ActivityMainBinding
+import edu.califer.facemaskdectector.ml.FackMaskDetection
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.label.Category
+import org.tensorflow.lite.support.model.Model
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
+
+typealias  CameraBitmapOutputListener = (bitmap: Bitmap) -> Unit
 
 class MainActivity : AppCompatActivity() {
+
+    private var preview: Preview? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var cameraProvider: ProcessCameraProvider? = null
+    private var lensFacing: Int = CameraSelector.LENS_FACING_FRONT
+    private var camera: Camera? = null
+    private lateinit var cameraExecutor: ExecutorService
+
+    private lateinit var binding: ActivityMainBinding
+
+    @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Hide the status bar.
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        actionBar?.hide()
+
+        setUpML()
+
+        setUpCameraThread()
+
+        setUpCameraControllers()
+
+        if (!allPermissionGranted) {
+            requireCameraPermission()
+        } else {
+            setUpCamera()
+        }
+
     }
+
+    private fun setUpCameraThread() {
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun setUpCameraControllers() {
+
+        binding.cameraSwitch.setOnClickListener {
+            lensFacing = if (lensFacing == CameraSelector.LENS_FACING_FRONT) {
+                CameraSelector.LENS_FACING_BACK
+            } else {
+                CameraSelector.LENS_FACING_FRONT
+            }
+
+            setUpCameraUseCases()
+        }
+        try {
+            binding.cameraSwitch.isEnabled = hasBackCamera && hasFrontCamera
+        } catch (exception: CameraInfoUnavailableException) {
+            binding.cameraSwitch.isEnabled = false
+            exception.printStackTrace()
+        }
+    }
+
+    private fun requireCameraPermission() {
+        ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSION)
+    }
+
+    private fun grantedCameraPermission(requestCode: Int) {
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (allPermissionGranted) {
+                setUpCamera()
+            } else {
+                Toast.makeText(applicationContext, "Permission Not Granted !", Toast.LENGTH_LONG)
+                    .show()
+                finish()
+            }
+        }
+    }
+
+    private fun setUpCameraUseCases() {
+
+        val cameraSelector: CameraSelector =
+            CameraSelector.Builder().requireLensFacing(lensFacing).build()
+
+        val metrics: DisplayMetrics = DisplayMetrics().also {
+            binding.cameraView.display.getRealMetrics(it)
+        }
+        val rotation: Int = binding.cameraView.display.rotation
+        val screenAspectRatio: Int = aspectRatio(metrics.widthPixels, metrics.heightPixels)
+        preview = Preview.Builder()
+            .setTargetAspectRatio(screenAspectRatio)
+            .setTargetRotation(rotation)
+            .build()
+
+
+        imageAnalyzer = ImageAnalysis.Builder()
+            .setTargetAspectRatio(screenAspectRatio)
+            .setTargetAspectRatio(rotation)
+            .build()
+            .also {
+                it.setAnalyzer(
+                    cameraExecutor,
+                    BitmapOutPutAnalysis(applicationContext) { bitmap ->
+                        setUpMLOutput(bitmap)
+                    }
+                )
+            }
+
+        cameraProvider?.unbindAll()
+        try {
+            camera = cameraProvider?.bindToLifecycle(
+                this,
+                cameraSelector,
+                preview,
+                imageAnalyzer
+            )
+            preview?.setSurfaceProvider(binding.cameraView.surfaceProvider)
+        } catch (exception: Exception) {
+            Log.d(TAG, "USE CASE BINDING FAILURE", exception)
+        }
+
+
+    }
+
+    private fun setUpCamera() {
+
+        val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
+            ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+
+            cameraProvider = cameraProviderFuture.get()
+
+            lensFacing = when {
+                hasFrontCamera -> CameraSelector.LENS_FACING_FRONT
+                hasBackCamera -> CameraSelector.LENS_FACING_BACK
+                else -> throw IllegalStateException("No Camera available")
+            }
+
+            setUpCameraControllers()
+            setUpCameraUseCases()
+        }, ContextCompat.getMainExecutor(this))
+
+    }
+
+    private val allPermissionGranted: Boolean
+        get() {
+            return REQUIRED_PERMISSIONS.all {
+                ContextCompat.checkSelfPermission(
+                    baseContext,
+                    it
+                ) == PackageManager.PERMISSION_GRANTED
+            }
+        }
+
+    private val hasBackCamera: Boolean
+        get() {
+            return cameraProvider?.hasCamera(CameraSelector.DEFAULT_BACK_CAMERA) ?: false
+        }
+
+    private val hasFrontCamera: Boolean
+        get() {
+            return cameraProvider?.hasCamera(CameraSelector.DEFAULT_FRONT_CAMERA) ?: false
+        }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        grantedCameraPermission(requestCode)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        setUpCameraControllers()
+    }
+
+    private fun aspectRatio(width: Int, height: Int): Int {
+        val previewRatio: Double = max(width, height).toDouble() / min(width, height)
+        if (abs(previewRatio - RATIO_4_3_VALUE) <= abs(previewRatio - RATIO_16_9_VALUE)) {
+            return AspectRatio.RATIO_4_3
+        }
+        return AspectRatio.RATIO_16_9
+    }
+
+
+    private lateinit var faceMaskDetection: FackMaskDetection
+
+
+    private fun setUpML() {
+        val options: Model.Options =
+            Model.Options.Builder().setDevice(Model.Device.GPU).setNumThreads(5).build()
+        faceMaskDetection = FackMaskDetection.newInstance(applicationContext, options)
+    }
+
+    private fun setUpMLOutput(bitmap: Bitmap) {
+        val tensorImage: TensorImage = TensorImage.fromBitmap(bitmap)
+        val result: FackMaskDetection.Outputs = faceMaskDetection.process(tensorImage)
+        val output: List<Category> = result.probabilityAsCategoryList.apply {
+            sortByDescending { res -> res.score }
+        }
+
+        lifecycleScope.launch(Dispatchers.Main) {
+            output.firstOrNull()?.let { category ->
+                binding.textView.text = category.label
+                binding.textView.setTextColor(
+                    ContextCompat.getColor(
+                        applicationContext,
+                        if (category.label == "without_mask") R.color.red else R.color.green
+                    )
+                )
+
+                binding.overlay.background = AppCompatResources.getDrawable(
+                    applicationContext,
+                    if (category.label == "without_mask") R.drawable.red_border else R.drawable.green_border
+                )
+
+                binding.progressBar.progressTintList = AppCompatResources.getColorStateList(
+                    applicationContext,
+                    if (category.label == "without_mask") R.color.red else R.color.green
+                )
+
+                binding.progressBar.progress = (category.score * 100).toInt()
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "Face_Mask_Detector"
+        private const val REQUEST_CODE_PERMISSION = 0x98
+        private val REQUIRED_PERMISSIONS: Array<String> =
+            arrayOf(android.Manifest.permission.CAMERA)
+        private const val RATIO_4_3_VALUE: Double = 4.0 / 3.0
+        private const val RATIO_16_9_VALUE: Double = (16.0 / 9.0)
+
+    }
+
+}
+
+private class BitmapOutPutAnalysis(
+    context: Context,
+    private val listener: CameraBitmapOutputListener
+) : ImageAnalysis.Analyzer {
+    private val yuvToRgbConverter = YuvToRgbConverter(context)
+    private lateinit var bitmapBuffer: Bitmap
+    private lateinit var rotationMatrix: Matrix
+
+    @SuppressLint("UnsafeExperimentalError", "UnsafeOptInUsageError")
+    private fun ImageProxy.toBitmap(): Bitmap? {
+
+        val image: Image = this.image ?: return null
+        if (!::bitmapBuffer.isInitialized) {
+            rotationMatrix = Matrix()
+            rotationMatrix.postRotate(this.imageInfo.rotationDegrees.toFloat())
+            bitmapBuffer = Bitmap.createBitmap(this.width, this.height, Bitmap.Config.ARGB_8888)
+        }
+
+        yuvToRgbConverter.yuvToRgb(image, bitmapBuffer)
+
+        return Bitmap.createBitmap(
+            bitmapBuffer,
+            0,
+            0,
+            bitmapBuffer.width,
+            bitmapBuffer.height,
+            rotationMatrix,
+            false
+        )
+
+    }
+
+    override fun analyze(image: ImageProxy) {
+        image.toBitmap()?.let {
+            listener(it)
+        }
+        image.close()
+    }
+
 }
---
+++ app/src/main/java/edu/califer/facemaskdectector/YuvToRgbConverter.kt
@@ -0,0 +1,137 @@
+package edu.califer.facemaskdectector
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageFormat
+import android.graphics.Rect
+import android.media.Image
+import android.renderscript.*
+import androidx.viewbinding.BuildConfig
+import java.nio.ByteBuffer
+
+/**
+ * This is a helper class which will be used to convert an image object from image format by YUV_420-888 to bitmap object.
+ * @param context The Context of the calling View.
+ */
+class YuvToRgbConverter(context: Context) {
+
+    private val renderString = RenderScript.create(context)
+
+    private val scriptYUVToRGB =
+        ScriptIntrinsicYuvToRGB.create(renderString, Element.U8_4(renderString))
+
+    private lateinit var YUBBuffer: ByteBuffer
+    private lateinit var inputAllocation: Allocation
+    private lateinit var outputAllocation: Allocation
+
+    //Pixel Count = 30fs and 640*480 px. This is the default analyzer for machine learning model
+    private var pixelCount: Int = -1
+
+    /**
+     * This function is able to achieve the same fps as the cameraX image analyses use case on a pixel 3XL device at a default analyser resolution which is 30fps with 640* 480 px.
+     */
+    @Synchronized
+    fun yuvToRgb(image: Image, output: Bitmap) {
+
+        if (!::YUBBuffer.isInitialized) {
+            pixelCount = image.cropRect.width() * image.cropRect.height()
+            val pixelSizeBits = ImageFormat.getBitsPerPixel(ImageFormat.YUV_420_888)
+            YUBBuffer = ByteBuffer.allocateDirect(pixelCount * pixelSizeBits / 8)
+        }
+        YUBBuffer.rewind()
+        imageToByteBuffer(image, YUBBuffer.array())
+
+        if (!::inputAllocation.isInitialized) {
+            val elementType =
+                Type.Builder(renderString, Element.YUV(renderString)).setYuvFormat(ImageFormat.NV21)
+                    .create()
+            inputAllocation =
+                Allocation.createSized(renderString, elementType.element, YUBBuffer.array().size)
+        }
+
+        if (!::outputAllocation.isInitialized) {
+            outputAllocation = Allocation.createFromBitmap(renderString, output)
+        }
+
+        inputAllocation.copyFrom(YUBBuffer.array())
+        scriptYUVToRGB.setInput(inputAllocation)
+        scriptYUVToRGB.forEach(outputAllocation)
+        outputAllocation.copyTo(output)
+
+    }
+
+    /**
+     *
+     */
+    private fun imageToByteBuffer(image: Image, outputBuffer: ByteArray) {
+
+        if (BuildConfig.DEBUG && image.format != ImageFormat.YUV_420_888) {
+            error("Assertion Failure")
+        }
+        val imageCrop = image.cropRect
+        val imagePlanes = image.planes
+        imagePlanes.forEachIndexed { index, plane ->
+
+            val outputStride: Int
+            var outputOffSet: Int
+            when (index) {
+                0 -> {
+                    outputStride = 1
+                    outputOffSet = 0
+                }
+                1 -> {
+                    outputStride = 2
+                    outputOffSet = pixelCount + 1
+                }
+                2 -> {
+                    outputStride = 2
+                    outputOffSet = pixelCount
+                }
+                else -> {
+                    return@forEachIndexed
+                }
+            }
+
+            val planeBuffer = plane.buffer
+            val rowStride = plane.rowStride
+            val pixelStride = plane.pixelStride
+            val planeCrop = if (index == 0) {
+                imageCrop
+            } else {
+                Rect(
+                    imageCrop.left / 2,
+                    imageCrop.top / 2,
+                    imageCrop.right / 2,
+                    imageCrop.bottom / 2
+                )
+            }
+
+            val planeWidth = planeCrop.width()
+            val planeHeight = planeCrop.height()
+            val rowBuffer = ByteArray(plane.rowStride)
+            val rowLength = if (pixelStride == 1 && outputStride == 1) {
+                planeWidth
+            } else {
+                (planeWidth - 1) * pixelStride + 1
+            }
+
+            for (row in 0 until planeHeight) {
+
+                planeBuffer.position(
+                    (row + planeCrop.top) * rowStride + planeCrop.left * pixelStride
+                )
+
+                if (pixelStride == 1 && outputStride == 1) {
+                    planeBuffer.get(outputBuffer, outputOffSet, rowLength)
+                    outputOffSet += rowLength
+                } else {
+                    planeBuffer.get(rowBuffer, 0, rowLength)
+                    for (col in 0 until planeWidth) {
+                        outputBuffer[outputOffSet] = rowBuffer[col * pixelStride]
+                        outputOffSet += outputStride
+                    }
+                }
+            }
+        }
+    }
+}
